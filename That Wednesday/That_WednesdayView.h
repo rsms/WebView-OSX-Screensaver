b7e6a5bfc13ee1972457ed20cb645830a475bf27
Rasmus Andersson <rasmus@notion.se>
2014-02-19 10:36:05 -0800

    Initial Commit

---
+++ That Wednesday/That_WednesdayView.h
@@ -0,0 +1,13 @@
+//
+//  That_WednesdayView.h
+//  That Wednesday
+//
+//  Created by Rasmus Andersson on 2/19/14.
+//  Copyright (c) 2014 Rasmus Andersson. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface That_WednesdayView : ScreenSaverView
+
+@end
